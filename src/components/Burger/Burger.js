0a72bd2a98c0c7db9956f6b749422b0e8ced151b
Dragos <dragos23vatamanu@gmail.com>
2019-12-18 18:27:26 +0200

    output burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,26 @@ import styles from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    // We are taking the state Object and turning it into an array,
+    // So we can then figure out the content of the burger,
+    // And display it dynamically using Array.map()
+
+    const transformedIngredients = Object.keys(props.ingredients)
+    // ingKey = ingredientKey
+    .map(ingKey => {
+        return [...Array(props.ingredients[ingKey])].map((_, i) => {
+            return <BurgerIngredient key={ingKey + i} type={ingKey} />
+        });
+    });
     return (
         <div className={styles.Burger}>
+            {/*
+            There will always be bread-bottom and bread-top
+            What the burger contains, cheese/meat/salad/bacon, that needs to be rendered dynamically
+            we use "transformedIngredients" for that, as explained before it's declaration.
+            */}
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
